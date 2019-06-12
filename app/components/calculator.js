03c598a9bb736ecf503f944a7add44a2df2cb16a
Dan Lubbers <lubbers.dan@gmail.com>
2019-06-11 20:00:33 -0400

    calculator functions are working

--- app/components/calculator.js
+++ app/components/calculator.js
@@ -2,6 +2,9 @@ import Component from '@ember/component';
 
 export default Component.extend({
     onDisplay: '0',
+    firstNum: '',
+    secondNum: '',
+    operator: '',
 
     actions: {
 
@@ -11,167 +14,93 @@ export default Component.extend({
             console.log(e.target.textContent);
             // This will be used to clear state
             this.set('onDisplay', '0');
-            console.log(this.onDisplay)
+            console.log(this.onDisplay);
         },
 
-
         // Number Keys
-
         numbers(e) {
             let key = e.target;
             let action = key.dataset.action;
             let keyContent = key.textContent;
-            let displayNum = e.target.textContent;
-            console.log(e)
-            console.log(displayNum)
-            // console.log(key, action, keyContent, onDisplay)
             
             if(action === 'number') {
                 if(this.onDisplay === '0') {
-                    this.set('onDisplay', displayNum);
+                    this.set('onDisplay', keyContent);
                 } else {
                     this.set('onDisplay', this.onDisplay + keyContent);
                 }
             }
-
         },
+
+        // Decimal Key
 
         decimal(e) {
             let key = e.target;
             let action = key.dataset.action;
             let keyContent = key.textContent;
-            let displayNum = e.target.textContent;
-            if(action === 'decimal') {
+    
+            // this checks if the action is equal to decimal and only adds a decimal to the display if there is not one already found. This prevents more than one decimal point being added. 
+            if(action === 'decimal' && !/\./g.test(this.onDisplay)) {
                 this.set('onDisplay', this.onDisplay + keyContent);
             }
         },
 
-        // zero(e) {
-        //     console.log(e.target.dataset.action);
-        //     // this.set('currentNum', e.target.textContent);
-        //     this.set('onDisplay', this.onDisplay + '0');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-        // one(e) {
-        //     console.log(e.target.dataset.action);
-        //         // this.set('currentNum', this.currentNum + '1');
-        //     this.set('onDisplay', this.onDisplay + '1');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-        // two(e) {
-        //     console.log(e.target.dataset.action);
-        //     // this.set('currentNum', e.target.textContent);
-        //     this.set('onDisplay', this.onDisplay + '2');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-        // three(e) {
-        //     console.log(e.target.dataset.action);
-        //     // this.set('currentNum', e.target.textContent);
-        //     this.set('onDisplay', this.onDisplay + '3');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-        // four(e) {
-        //     console.log(e.target.dataset.action);
-        //     // this.set('currentNum', e.target.textContent);
-        //     this.set('onDisplay', this.onDisplay + '4');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-        // five(e) {
-        //     console.log(e.target.dataset.action);
-        //     // this.set('currentNum', e.target.textContent);
-        //     this.set('onDisplay', this.onDisplay + '5');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-        // six(e) {
-        //     console.log(e.target.dataset.action);
-        //     // this.set('currentNum', e.target.textContent);
-        //     this.set('onDisplay', this.onDisplay + '6');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-        // seven(e) {
-        //     console.log(e.target.dataset.action);
-        //     // this.set('currentNum', e.target.textContent);
-        //     this.set('onDisplay', this.onDisplay + '7');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-        // eight(e) {
-        //     console.log(e.target.dataset.action);
-        //     // this.set('currentNum', e.target.textContent);
-        //     this.set('onDisplay', this.onDisplay + '8');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-        // nine(e) {
-        //     console.log(e.target.dataset.action);
-        //     // this.set('currentNum', e.target.textContent);
-        //     this.set('onDisplay', this.onDisplay + '9');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-
-        // Decimal Key
-
-        // decimal(e) {
-        //     console.log(e.target.dataset.action);
-        //     this.set('currentNum', this.currentNum + '.');
-        //     this.set('onDisplay', this.onDisplay + '.');
-        //     console.log(this.currentNum, this.onDisplay);
-        // },
-
-
         // Operand Keys
 
-        add(e) {
-            console.log(e.target.dataset.action);
-            if (e.target.dataset.action === 'operand') {
-
+        operator(e) {
+            let key = e.target;
+            let action = key.dataset.action;
+            let keyContent = key.textContent;
+            if(action === 'operator') {
+                this.set('firstNum', this.onDisplay);
+                this.set('onDisplay', '0');
+                this.set('operator', keyContent);
+                console.log(this.firstNum);
+                console.log(this.operator);
             }
-            
         },
 
+        // Calculations
 
-        subtract(e) {
-            console.log(e.target.dataset.action);
-            console.log('subtract');
-        },
-
-
-        multiply(e) {
-            console.log(e.target.dataset.action);
-            console.log('multiply');
-        },
-
-
-        divide(e) {
-            console.log(e.target.dataset.action);
-            console.log('divide');
-        },
-
-        
-        // Equal Key
-        
         equals(e) {
-            console.log(e.target.dataset.action);
-            console.log('equals');
-        },
+            let key = e.target;
+            let keyContent = key.textContent;
+            this.set('secondNum', this.onDisplay);
+            console.log(keyContent);
 
+            if(this.operator === '+') {
+                let addition = +this.firstNum + +this.secondNum;
+                this.set('onDisplay', addition);
+                console.log(addition);
+            }
+
+            if(this.operator === '-') {
+                let subtraction = +this.firstNum - +this.secondNum;
+                this.set('onDisplay', subtraction);
+                console.log(subtraction);
+            }
+
+            if(this.operator === 'x') {
+                let multiplication = +this.firstNum * +this.secondNum;
+                this.set('onDisplay', multiplication); 
+                console.log(multiplication);
+            }
+
+            if(this.operator === '÷') {
+                let division = +this.firstNum / +this.secondNum;
+                this.set('onDisplay', division);
+                console.log(division);
+            }
+        },
 
         // Extras
         
         plusMinus() {
-            console.log('plus-minus');
+            alert('Plus/Minus, I currently do not work');
         },
         
         percent() {
-            console.log('percent');
+            alert('Percent, I currently do not work');
         },
     }
 });
