407d910e0c3ba5a6d330ef85efcedd7958255cdf
Dan Lubbers <lubbers.dan@gmail.com>
2019-06-14 11:05:51 -0400

    added some notes | starting on testing

--- app/components/calculator.js
+++ app/components/calculator.js
@@ -1,100 +1,121 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    // This shows what we see on screen in the Display
     onDisplay: '0',
+    // This sets the first number before hitting an operator
     firstNum: '',
+    // This sets the second number after hitting operator and before hitting equals
     secondNum: '',
+    // This sets the operator 
     operator: '',
 
     actions: {
 
         // Clear Key
-
         clickAC(e) {
-            console.log(e.target.textContent);
-            // This will be used to clear state
+            // console.log(e.target.textContent);
+            // This will be used to clear/reset state to 0
             this.set('onDisplay', '0');
-            console.log(this.onDisplay);
+            // console.log(this.onDisplay);
         },
 
         // Number Keys
         numbers(e) {
+            // key shows what is in the <td> property
             let key = e.target;
+            // console.log(key);
+
+            // action shows what value is as the data-action
             let action = key.dataset.action;
+            // console.log(action);
+
+            // keyContent shows the text content of the <td>
             let keyContent = key.textContent;
+            // console.log(keyContent);
             
             if(action === 'number') {
                 if(this.onDisplay === '0') {
+                    // If 0 is showing in the display, this will change state to the current keyContent that was clicked
                     this.set('onDisplay', keyContent);
                 } else {
+                    // If 0 is not showing in the display, the keyContent will be appended to the number that is onDisplay
                     this.set('onDisplay', this.onDisplay + keyContent);
                 }
             }
         },
 
         // Decimal Key
-
         decimal(e) {
             let key = e.target;
             let action = key.dataset.action;
             let keyContent = key.textContent;
     
-            // this checks if the action is equal to decimal and only adds a decimal to the display if there is not one already found. This prevents more than one decimal point being added. 
+            // this checks if the action is equal to decimal and only adds a decimal to the display if there is not one already found. This prevents more than one decimal point being added. I used regex, but I could have also used .includes('.')
             if(action === 'decimal' && !/\./g.test(this.onDisplay)) {
                 this.set('onDisplay', this.onDisplay + keyContent);
             }
         },
 
-        // Operand Keys
-
+        // Operator Keys
         operator(e) {
             let key = e.target;
             let action = key.dataset.action;
             let keyContent = key.textContent;
+
             if(action === 'operator') {
+                // Once an operator is clicked, it will set what is onDisplay to the firstNum variable in state.
                 this.set('firstNum', this.onDisplay);
-                this.set('onDisplay', '0');
+                // This clears the display between the first number and second number being chosen
+                this.set('onDisplay', '');
+                // This will set the operator variable to the correct operator based on which button was clicked.
                 this.set('operator', keyContent);
-                console.log(this.firstNum);
-                console.log(this.operator);
+                // console.log(this.firstNum);
+                // console.log(this.operator);
             }
         },
 
         // Calculations
-
         equals(e) {
-            let key = e.target;
-            let keyContent = key.textContent;
-            this.set('secondNum', this.onDisplay);
-            console.log(keyContent);
+            // let key = e.target;
+            // let keyContent = key.textContent;
 
+            // Once the equals button is clicked it sets what is onDisplay to the secondNum variable 
+            // Now we can use our firstNum, secondNum and operator variables to make the calculations
+            this.set('secondNum', this.onDisplay);
+            // console.log(keyContent);
+
+            // Addition
             if(this.operator === '+') {
+                // The plus (+) I used to turn the string into a number instead of ParseFloat
                 let addition = +this.firstNum + +this.secondNum;
                 this.set('onDisplay', addition);
-                console.log(addition);
+                // console.log(addition);
             }
 
+            // Subtraction
             if(this.operator === '-') {
                 let subtraction = +this.firstNum - +this.secondNum;
                 this.set('onDisplay', subtraction);
-                console.log(subtraction);
+                // console.log(subtraction);
             }
 
+            // Multiplication
             if(this.operator === 'x') {
                 let multiplication = +this.firstNum * +this.secondNum;
                 this.set('onDisplay', multiplication); 
-                console.log(multiplication);
+                // console.log(multiplication);
             }
 
+            // Division
             if(this.operator === '÷') {
                 let division = +this.firstNum / +this.secondNum;
                 this.set('onDisplay', division);
-                console.log(division);
+                // console.log(division);
             }
         },
 
-        // Extras
-        
+        // Extras - I did not get to these!
         plusMinus() {
             alert('Plus/Minus, I currently do not work');
         },
--- app/routes/themes.js
+++ app/routes/themes.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
+        // Error: themes Model is not defined ReferenceError: Model is not defined
         // return this.store.findAll('themes');
     },
 
